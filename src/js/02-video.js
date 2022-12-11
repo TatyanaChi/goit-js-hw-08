ac161eae23dc88ca302e83c405211fe07062164b
TatyanaChi <tischenko3004@gmail.com>
2022-12-11 23:14:25 +0100

    add video-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  })
+);
+const data = localStorage.getItem('videoplayer-current-time');
+const parsed = JSON.parse(data);
+player
+  .setCurrentTime(parsed)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });

b2902626a14c91f0cfdfc083ccd2ee948a895a3d
TatyanaChi <tischenko3004@gmail.com>
2022-12-12 02:05:26 +0100

    add feedback-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
 
+const refs = {
+  formRef: document.querySelector('.feedback-form'),
+  inputEmailRef: document.querySelector('input[type="email"]'),
+  inputMessageRef: document.querySelector('input[name="message"]'),
+};
+
+refs.formRef.addEventListener('input', throttle(onFormInput, 500));
+refs.formRef.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
